91e0bafd30d20bbf3557dec98853560196e8f72a
Pedro <pedro.chn01@gmail.com>
2022-08-02 12:28:28 -0700

    5th test passing

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,8 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+
+SELECT title FROM customer 
+LEFT JOIN rental ON customer.customer_id = rental.customer_id
+LEFT JOIN inventory ON rental.inventory_id = inventory.inventory_id
+LEFT JOIN film ON inventory.film_id = film.film_id
+WHERE customer.first_name = 'Roberta'
