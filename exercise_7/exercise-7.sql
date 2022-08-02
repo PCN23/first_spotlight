5f0823ae959750fa040c414a221323931179596f
Pedro <pedro.chn01@gmail.com>
2022-08-02 12:30:41 -0700

    7th passing

--- exercise_7/exercise-7.sql
+++ exercise_7/exercise-7.sql
@@ -1,3 +1,11 @@
 -- find all film titles rented out by customers who live in the city of Dundee
 -- Similar to exercise 6 but you need a few more joins to get the city name
 
+
+SELECT title FROM customer 
+LEFT JOIN rental ON customer.customer_id = rental.customer_id
+LEFT JOIN inventory ON rental.inventory_id = inventory.inventory_id
+LEFT JOIN film ON inventory.film_id = film.film_id
+LEFT JOIN address ON customer.address_id = address.address_id
+LEFT JOIN city ON address.city_id = city.city_id
+WHERE city.city = 'Dundee'
