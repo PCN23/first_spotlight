1f755221662aa430f4e2b7e303c48cf70f35b023
Pedro <pedro.chn01@gmail.com>
2022-08-02 12:44:03 -0700

    8th test passing

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,11 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+
+SELECT title, COUNT(inventory.store_id) 
+FROM rental 
+LEFT JOIN inventory ON rental.inventory_id = inventory.inventory_id
+LEFT JOIN film ON inventory.film_id = film.film_id
+GROUP BY film.film_id
+ORDER BY COUNT DESC
+LIMIT 10
